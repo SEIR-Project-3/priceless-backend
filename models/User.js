0d7132629ce33bf9f0bd8711e06dded8d4a54348
ekobara <emmanuelkobara@gmail.com>
2021-10-06 20:35:53 -0400

    Updatted User model for authentication

--- models/User.js
+++ models/User.js
@@ -1,17 +1,39 @@
 const mongoose = require('../db/connection');
 
-const UserSchema = new mongoose.Schema({
-	username: String,
-	email: String,
-	password: {
-		type: String,
-		required: true,
+const UserSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+
+		// items: {
+		// 	type: mongoose.Schema.Types.ObjectId,
+		// 	ref: 'Item',
+		// },
 	},
-	items: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Item',
-	},
-});
+	{
+		timestamps: true,
+    	toJSON: {
+      	virtuals: true,
+    	// ret is the returned Mongoose document
+    	transform: (_doc, ret) => {
+        	delete ret.password;
+        	return ret;
+      		},
+    	},
+  	}
+);
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
