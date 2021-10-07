b9bdf5e8abdfab160f376e40d178fb66ec4d864e
ekobara <emmanuelkobara@gmail.com>
2021-10-06 21:05:50 -0400

    Authentication file added. Signup & Signin requests added to user controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,24 +1,57 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const User = require('../models/User');
 
-router.get('/', async (req, res, next) => {
+
+const { createUserToken } = require('../middleware/auth');
+
+
+// router.get('/', async (req, res, next) => {
+// 	try {
+// 		const users = await User.find();
+// 		res.status(200).json(users);
+// 	} catch (error) {
+// 		next(error);
+// 	}
+// });
+
+
+router.get('/users', async (req, res, next) => {
 	try {
-		const users = await User.find();
+		const users = await User.find(req.body);
 		res.status(200).json(users);
 	} catch (error) {
 		next(error);
 	}
 });
 
-router.post('/users', async (req, res, next) => {
+
+// SIGN UP
+router.post('/signup', async (req, res, next) => {
 	try {
-		const newUser = await User.create(req.body);
-		res.status(201).json(newUser);
+		const password = await bcrypt.hash(req.body.password, 10);
+		const user = await User.create({
+			user: req.body.username,
+			email: req.body.email,
+			password
+		});
+	} catch (error) {
+		
+	}
+});
+
+// SIGN IN
+router.post('/signin', async (req, res, next) => {
+	try {
+		const user = await User.findOne({ user: req.body.username });
+		const token = await createUserToken(req, user);
+		res.json({ token })
 	} catch (error) {
 		next(error);
 	}
 });
+
 
 router.patch('/users/:id', async (req, res, next) => {
 	try {
