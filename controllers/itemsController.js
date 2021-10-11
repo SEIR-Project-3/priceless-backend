f141046273856cbaf574dd387fd1716a4a8e8d93
ekobara <emmanuelkobara@gmail.com>
2021-10-11 15:23:34 -0400

    Require token removed from CRUD functionality

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -32,7 +32,7 @@ router.get('/:id', async (req, res, next) => {
 	}
 });
 
-router.post('/', requireToken, async (requireToken, req, res, next) => {
+router.post('/', async (req, res, next) => {
 	try {
 		const newItem = await Item.create(req.body);
 		res.status(201).json(newItem);
@@ -41,7 +41,7 @@ router.post('/', requireToken, async (requireToken, req, res, next) => {
 	}
 });
 
-router.put('/:id', requireToken, async (requireToken, req, res, next) => {
+router.put('/:id', async (req, res, next) => {
 	try {
 		const id = req.params.id;
 		const item = await Item.findByIdAndUpdate(id, req.body, {new:true});
@@ -51,18 +51,14 @@ router.put('/:id', requireToken, async (requireToken, req, res, next) => {
 	}
 });
 
-router.delete(
-	'/:id',
-	requireToken,
-	async (requireToken, req, res, next) => {
-		try {
-			const id = req.params.id;
-			const deleted = await Item.findByIdAndDelete({_id : id });
-			res.json(deleted);
-		} catch (error) {
-			next(error);
-		}
+router.delete('/:id', async (req, res, next) => {
+	try {
+		const id = req.params.id;
+		const deleted = await Item.findByIdAndDelete({_id : id });
+		res.json(deleted);
+	} catch (error) {
+		next(error);
 	}
-);
+});
 
 module.exports = router;
