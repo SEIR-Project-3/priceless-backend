3b67f631321c167f6e56e759cee49fe5f4656d1a
itsreallydrew <a.mclean2526@gmail.com>
2021-10-13 08:26:41 -0500

    updated readme

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -4,25 +4,30 @@ const Item = require('../models/Item');
 
 const { requireToken } = require('../middleware/auth');
 
+// GET request for all items and populate owner field
 router.get('/', async (req, res, next) => {
 	try {
-		const item = await Item.find().populate({path: "owner", select: "username email"} );
+		const item = await Item.find().populate({
+			path: 'owner',
+			select: 'username email',
+		});
 		res.status(200).json(item);
 	} catch (error) {
 		next(error);
 	}
 });
 
+// GET items owned by specific user
 router.get('/user/:id', async (req, res, next) => {
 	try {
-		const items = await Item.find({owner: {_id: req.params.id }});
+		const items = await Item.find({ owner: { _id: req.params.id } });
 		res.json(items);
 	} catch (error) {
 		next(error);
 	}
 });
 
-
+// GET item by ID
 router.get('/:id', async (req, res, next) => {
 	try {
 		const item = await Item.findById(req.params.id);
@@ -32,6 +37,7 @@ router.get('/:id', async (req, res, next) => {
 	}
 });
 
+// POST to create new item
 router.post('/', async (req, res, next) => {
 	try {
 		const newItem = await Item.create(req.body);
@@ -41,20 +47,22 @@ router.post('/', async (req, res, next) => {
 	}
 });
 
+// PUT to update item info
 router.put('/:id', async (req, res, next) => {
 	try {
 		const id = req.params.id;
-		const item = await Item.findByIdAndUpdate(id, req.body, {new:true});
+		const item = await Item.findByIdAndUpdate(id, req.body, { new: true });
 		res.status(200).json(item);
 	} catch (error) {
 		next(error);
 	}
 });
 
+// DELETE item
 router.delete('/:id', async (req, res, next) => {
 	try {
 		const id = req.params.id;
-		const deleted = await Item.findByIdAndDelete({_id : id });
+		const deleted = await Item.findByIdAndDelete({ _id: id });
 		res.json(deleted);
 	} catch (error) {
 		next(error);
